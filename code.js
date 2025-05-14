56eec7eb59f27b9d72732bb90e6de00876448ef7
Sohal-03 <sohal.learn.it@gmail.com>
2025-05-14 01:49:35 -0600

    Added an alert on website homepage
    Alert tells about the current state of the website, at this given time, it is "Not Completed" and "No dynamic links setup".

---
+++ code.js
@@ -0,0 +1,3 @@
+alert(
+  'This page is for learning purposes only, NOT completed yet.\nWorking on my coding skills right now for a project :)\nThe dynamic links have not been setup as of right now'
+);
